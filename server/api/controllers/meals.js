9fdc858e2a8decea6240f28f0d5221ad9f56b87a
oba <thevetdoctor@gmail.com>
2019-02-15 01:53:51 +0100

    [finishes #163845630]

---
+++ server/api/controllers/meals.js
@@ -0,0 +1,79 @@
+// api/controllers/meals.js
+
+import mealsRecord from '../models/meals';
+
+const mealsController = {
+  addMeal: (req, res) => {
+    const meal = {
+      id: mealsRecord.length,
+      name: req.body.name,
+      price: req.body.price,
+    };
+    if (meal) {
+      meal.id = mealsRecord.length + 1;
+      mealsRecord.push(meal);
+      res.status(201).json({
+        status: 201,
+        data: {
+          id: meal.id,
+          name: meal.name,
+          price: meal.price,
+        },
+        message: 'New Meal Added',
+      });
+    } else {
+      res.status(404).json({
+        error: 'Invalid input',
+      });
+    }
+  },
+
+  modifyMeal: (req, res) => {
+    const mealId = parseInt(req.params.id, 10);
+
+    if (mealsRecord.length > 0) {
+      const findingMeal = meal => meal.id === mealId;
+
+      const foundMeal = mealsRecord.find(findingMeal);
+      foundMeal.name = req.body.name;
+      foundMeal.price = req.body.price;
+
+      res.status(200).json({
+        status: 200,
+        data: {
+          id: foundMeal.id,
+          name: foundMeal.name,
+          price: foundMeal.price,
+        },
+        message: `meal with ID ${foundMeal.id} modified`,
+      });
+    } else {
+      res.status(404).json({
+        error: 'Meal not modified',
+      });
+    }
+  },
+
+  deleteMeal: (req, res) => {
+    const mealId = parseInt(req.params.id, 10);
+
+    if (mealsRecord.length > 0) {
+      const findingMeal = meal => meal.id === mealId;
+
+      const foundMeal = mealsRecord.find(findingMeal);
+
+      mealsRecord.splice(foundMeal.id - 1, 1);
+
+      res.status(200).json({
+        status: 200,
+        message: `Meal with ID ${foundMeal.id} deleted`,
+      });
+    } else {
+      res.status(404).json({
+        error: 'Meal not deleted',
+      });
+    }
+  },
+};
+
+export default mealsController;
---
+++ server/api/models/meals.js
@@ -0,0 +1,65 @@
+// api/models/meals.js
+
+const mealsRecord = [
+  {
+    id: 1,
+    name: 'White Rice & Chicken',
+    price: '30.00',
+  },
+  {
+    id: 2,
+    name: 'Jollof Rice & Chicken',
+    price: '40.00',
+  },
+  {
+    id: 3,
+    name: 'Beans & Fried Plantain',
+    price: '35.00',
+  },
+  {
+    id: 4,
+    name: 'Boiled Yam & Egg Sauce',
+    price: '30.00',
+  },
+  {
+    id: 5,
+    name: 'Toasted Bread & Egg Sauce',
+    price: '25.00',
+  },
+  {
+    id: 6,
+    name: 'Indomie Special',
+    price: '20.00',
+  },
+  {
+    id: 7,
+    name: 'Fufu & Vegetable Soup',
+    price: '40.00',
+  },
+  {
+    id: 8,
+    name: 'Amala & Ewedu Soup',
+    price: '40.00',
+  },
+  {
+    id: 9,
+    name: 'Eba & Egusi Soup',
+    price: '40.00',
+  },
+  {
+    id: 10,
+    name: 'Yam Porridge',
+    price: '30.00',
+  },
+  {
+    id: 11,
+    name: 'Fried Rice & Chicken',
+    price: '35.00',
+  },
+  {
+    id: 12,
+    name: 'Catfish Peppersoup',
+    price: '40.00',
+  }];
+
+export default mealsRecord;
