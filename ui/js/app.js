a6bd63794258905a166d387e9704abc392fb28fd
oba <thevetdoctor@gmail.com>
2019-02-16 10:56:49 +0100

    ui-updates

---
+++ ui/js/app.js
@@ -0,0 +1,7 @@
+const dropdown = document.querySelector('.dropdown');
+const dropdownContent = document.querySelector('.dropdown-content');
+
+dropdown.addEventListener('mouseover', () => {
+    dropdownContent.style.display = 'block';
+})
+
