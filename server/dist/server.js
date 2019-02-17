8d31f064433bc967957ef42f863acb02bc6932ae
oba <thevetdoctor@gmail.com>
2019-02-17 20:49:25 +0100

    same travis palava

--- server/dist/server.js
+++ server/dist/server.js
@@ -24,32 +24,39 @@ var _orders = require('../api/routes/orders');
 
 var _orders2 = _interopRequireDefault(_orders);
 
+var _users = require('../api/routes/users');
+
+var _users2 = _interopRequireDefault(_users);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// import path from 'path';
+/* eslint-disable no-console */
 var app = (0, _express2.default)();
 
 app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: true }));
 
+// app.use(express.static(path.join(__dirname, '/ui')));
+app.use(_express2.default.static(__dirname.replace('server\\src', 'ui')));
+
 app.use('/api/v1/meals', _meals2.default);
 app.use('/api/v1/menus', _menus2.default);
 app.use('/api/v1/orders', _orders2.default);
-
-var meal = function meal(req, res) {
-  res.end('<h1> Check out Book-A-Meal, and let\'s give you a sumptuous experience!</h1>\n  <h2> Check out Book-A-Meal, and let\'s give you a sumptuous experience!</h2>\n  <h3> Check out Book-A-Meal, and let\'s give you a sumptuous experience!</h3>\n  <h4> Check out Book-A-Meal, and let\'s give you a sumptuous experience!</h4>');
-};
-
-app.get('/meal', meal);
+app.use('/auth/users/', _users2.default);
 
 var port = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
   // res.json({name: `Welcome to Book-A-Meal ...Your satisfaction is much assured!`});
-  res.end('<h1> Welcome to Book-A-Meal </h1> <h3>...Your satisfaction is much assured! </h3>');
+  res.sendFile(__dirname.replace('server\\src', 'ui\\index.html'));
+  // res.end(`<h1> Welcome to Book-A-Meal </h1> <h3>...where your satisfaction is much assured! </h3>
+  //           Check out our array of delicacies, and let's give you a sumptuous experience!`);
 });
 
 app.listen(port, function () {
-  //  console.log(`Server started at port ${port} for Book-A-Meal App!`);
+  console.log('Server started at port ' + port + ' for Book-A-Meal App!');
+  console.log(__dirname.replace('server\\src', 'ui\\index.html'));
 });
 
 exports.default = app;
